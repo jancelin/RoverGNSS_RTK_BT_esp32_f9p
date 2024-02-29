63bbb0979314a1ec35ecfc6be44f83686da9a811
jancelin <julien.ancelin@inra.fr>
2024-02-29 13:01:12 +0100

    less cable upd f9p receiver over wifi with U-center

---
+++ GNSS/receiverConf_WIFI-TCP-UART/receiverConf_WIFI-TCP-UART.ino
@@ -0,0 +1,99 @@
+#include <WiFi.h>
+  
+// debug log, set to 1 to enable
+#define ENABLE_DEBUG_LOG 1
+  
+// wifi config
+const char* ssid     = "SSID-NAME";
+const char* password = "SSID-PSWD";
+  
+// ethernet config
+const IPAddress local_IP(192, 168, 70, 98);
+const IPAddress gateway(192, 168, 70, 99);
+const IPAddress subnet(255, 255, 255, 0);
+const IPAddress primaryDNS(8, 8, 8, 8);
+const IPAddress secondaryDNS(8, 8, 4, 4);
+  
+// rs-server config
+const int serverPort = 1234;
+  
+// rs port config
+const int baudrate = 115200;
+const int rs_config = SERIAL_8N1;
+  
+// reading buffor config
+#define BUFFER_SIZE 1024
+  
+// global objects
+WiFiServer server;
+byte buff[BUFFER_SIZE];
+  
+void debug_log(char* str) {
+#if ENABLE_DEBUG_LOG == 1
+   Serial.println(str);
+#endif
+}
+  
+void setup() {
+
+    // POWER_PIN : This pin controls the power supply of the SIM7600
+  pinMode(25, OUTPUT);
+  digitalWrite(25, HIGH);
+
+// init rs port
+   Serial.begin(baudrate, rs_config);
+   Serial1.begin(115200, SERIAL_8N1, 27, 26 /*, false, 20000UL, SERIAL_SIZE_RX*/);
+  
+// init wifi connection
+if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+       debug_log("Failed to configure network settings");
+}
+   WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) {
+       debug_log("connecting to WiFi network");
+       delay(500);
+}
+  
+#if ENABLE_DEBUG_LOG == 1
+   Serial.println("connected to WiFi");
+   Serial.println("IP adddr: ");
+   Serial.println(WiFi.localIP());
+#endif
+   delay(1000);
+  
+//start server
+   server = WiFiServer(serverPort);
+   server.begin();
+   delay(1000);
+   debug_log("server started");
+}
+  
+void loop() {
+// wait for client
+   WiFiClient client = server.available();
+if (!client)
+return;
+  
+   debug_log("client found");
+while (client.connected()) {
+int size = 0;
+  
+// read data from wifi client and send to serial1
+while ((size = client.available())) {
+           size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
+           client.read(buff, size);
+           Serial1.write(buff, size);
+           Serial1.flush();
+}
+  
+// read data from serial1 and send to wifi client
+while ((size = Serial1.available())) {
+           size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
+           Serial1.readBytes(buff, size);
+           client.write(buff, size);
+           client.flush();
+}
+}
+   debug_log("client disconnected");
+   client.stop();
+}
